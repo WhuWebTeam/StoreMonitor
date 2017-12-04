fbc8657ad37259e794b21c4a3f0d75d0c1b910a7
qwasxj <1942750262@qq.com>
2017-12-04 16:58:43 -0500

    modify eventsList api

--- app/router/eventsList.js
+++ app/router/eventsList.js
@@ -15,15 +15,17 @@ module.exports = app => {
     app.get('/api/v1/eventsList/countGraph/checker/:day', 'eventsList.getEventsListGraph'); // get statistics graph of eventsList about checker(day: 'day', 'month', 'week')
     app.get('/api/v1/eventsList/countGraph/manager/:userId/:day', 'eventsList.getManageDealCount'); // get statistics graph of eventsList about store manager and district manager(day, week, month)
     
-    app.get('/api/v1/eventsList/rate/graph/manager/:userId', 'eventsList.getErrorRateGraph'); // get the error rate graph statistic of shops
-    app.get('/api/v1/eventsList/rate/list/manager/:userId/:day', 'eventsList.getErrorRateList'); // get the error rate of list of shops(day: 'week', 'month', '3month', '6month')
+    app.get('/api/v1/eventsList/errorRate/graph/:userId', 'eventsList.getErrorRateGraph'); // get the error rate graph statistic of shops
+    app.get('/api/v1/eventsList/errorRate/list/:userId/:day', 'eventsList.getErrorRateList'); // get the error rate of list of shops(day: 'week', 'month', '3month', '6month')
     app.get('/api/v1/eventsList/rate/:userId/:day', 'eventsList.getEventsRate'); // get the rate of events count during some time(day: 'week', 'month', '3month', '6month')
 
-    app.get('/api/v1/eventsList/list/:status/:userId', 'eventsList.getEventListByStatus'); // get list of eventList record by status
+    app.get('/api/v1/eventsList/list/checker/common/:status/:userId', 'eventsList.getEventListByStatus'); // get list of eventList record by status
     app.get('/api/v1/eventsList/list/manager/:status/:userId', 'eventsList.getManageEventListByStatus'); // get list of store manager and district manager
-    app.get('/api/v1/eventsList/list/:status/:result', 'eventsList.getEventList'); // get list of eventList record by status and result used to filer
+    app.get('/api/v1/eventsList/list/checker/filter/:status/:result', 'eventsList.getEventList'); // get list of eventList record by status and result used to filer
+    
     app.get('/api/v1/eventsList/editInfo/:sysKey', 'eventsList.getEditInfo'); // get some event's edit information
-    app.put('/api/v1/eventsList/:sysKey', 'eventsList.eventEdit'); // modify some eventList's info
+    app.put('/api/v1/eventsList/editInfo/:sysKey', 'eventsList.eventEdit'); // modify some eventList's info
+    
     app.put('/api/v1/eventsList/status/commit/:sysKey', 'eventsList.commitEventList'); // commit some eventList, set its status to 2
     app.put('/api/v1/eventsList/status/store/:sysKey', 'eventsList.storeEventList'); // store some eventList, set its status to 1
     app.put('/api/v1/eventsList/status/commit', 'eventsList.commitEventsList'); // commit some eventsList, set their status to 2
